f3b10a83716fad6725a42f8b77f6de8731d3751d
zz-guo <guozhongzheng2017@ia.ac.cn>
2021-04-14 23:32:57 +0800

    add shellSort algorithm

--- sort.py
+++ sort.py
@@ -32,19 +32,42 @@ class sortMethod():
                 self.lst[tmpLst[-1][0]] = temp1
         return self.lst
 
-    def insertionSort(self):
+    def insertionSort(self, start=0, gap=1):
         """
         去纸上写 ！！
         :return:
         """
-        for i in range(1, len(self.lst)):
+        for i in range(start+gap, len(self.lst), gap):
             currentVal = self.lst[i]
             currentPos = i
-            while currentPos > 0:
-                if self.lst[currentPos-1] > currentVal:
-                    self.lst[currentPos] = self.lst[currentPos-1]
-                    self.lst[currentPos - 1] = currentVal
-                currentPos -= 1
+            while currentPos >= gap:
+                if self.lst[currentPos-gap] > currentVal:
+                    self.lst[currentPos] = self.lst[currentPos-gap]
+                    self.lst[currentPos - gap] = currentVal
+                currentPos -= gap
+        return self.lst
+
+    def shellInsertionSort(self, lst, start, gap):
+        for j in range(start + gap, len(lst), gap):
+            curVal = lst[j]
+            curPos = j
+
+            while curPos >= gap and lst[curPos - gap] > curVal:
+                lst[curPos] = lst[curPos - gap]
+                curPos = curPos - gap
+            lst[curPos] = curVal
+
+    def shellSort(self):
+        gap = len(self.lst) // 2
+        while gap > 0:
+
+            for i in range(gap):
+
+                # self.lst = self.insertionSort(start=i, gap=gap)
+                self.insertionSort(i, gap)
+            # print(gap, self.lst)
+            gap = gap // 2
+
         return self.lst
 
 
@@ -64,16 +87,23 @@ if __name__ == '__main__':
     # end2 = time.time()
 
 
-    # 3.524892807006836
-    start3 = time.time()
-    [sortMethod(lst).insertionSort() for _ in range(int(10e5))]
-    print(sortMethod(lst).insertionSort())
-    end3 = time.time()
+    # # 3.524892807006836
+    # start3 = time.time()
+    # [sortMethod(lst).insertionSort() for _ in range(int(10e5))]
+    # print(sortMethod(lst).insertionSort())
+    # end3 = time.time()
+
+    # 7.26346230506897
+    start4 = time.time()
+    [sortMethod(lst).shellSort() for _ in range(int(10e5))]
+    print(sortMethod(lst).shellSort())
+    end4 = time.time()
 
     print(
         # end1 - start1,
-        # end2 - start2
-        end3 - start3
+        # end2 - start2,
+        # end3 - start3,
+        end4 - start4
     )
     pass
 
