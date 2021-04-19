1deee50bf12f464b724682ba953d42c0dce9691e
zz-guo <guozhongzheng2017@ia.ac.cn>
2021-04-20 00:09:33 +0800

    build binary tree

---
+++ DST_Tree.py
@@ -0,0 +1,43 @@
+
+class BinaryTree():
+    def __init__(self, RootVal):
+        self.RootVal = RootVal
+        self.LeftChild = None
+        self.RightChild = None
+        pass
+
+    def InsertLeft(self, val):
+        t = BinaryTree(val)
+        if self.LeftChild is None:
+            self.LeftChild = t
+        else:
+            t.LeftChild = self.LeftChild
+            self.LeftChild = t
+        return self
+
+    def InsertRight(self, val):
+        t = BinaryTree(val)
+        if self.RightChild is None:
+            self.RightChild = t
+        else:
+            t.RightChild = self.RightChild
+            self.RightChild = t
+        return self
+
+    def GetLeftChild(self):
+        return self.LeftChild
+
+    def GetRightChild(self):
+        return self.RightChild
+
+    def GetRootVal(self):
+        return self.RootVal
+
+    def SetRootVal(self, NewVal):
+        self.RootVal = NewVal
+
+
+if __name__ == '__main__':
+    bt = BinaryTree(1).InsertLeft(3).InsertLeft(2).InsertRight(1.5)
+    print(bt.GetLeftChild().GetRootVal())
+    print()
