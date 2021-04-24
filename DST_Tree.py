b7c92f518f7ed3b8f205f5b93db4e73b6e95f37c
zz-guo <guozhongzheng2017@ia.ac.cn>
2021-04-24 22:25:30 +0800

    finish BinaryHeap

--- DST_Tree.py
+++ DST_Tree.py
@@ -1,7 +1,9 @@
 
 """
+See also DST_NonLinearTable.py
+
 二叉树
-二叉堆 （ 满二叉树 --> 平衡二叉树 ）
+二叉堆 （ 平衡： 满二叉树 --> 完全二叉树 ）
 二叉搜索树
 AVL树
 """
@@ -42,34 +44,37 @@ class BinaryTree():
     def SetRootVal(self, NewVal):
         self.RootVal = NewVal
 
-    def preorder(self, tree):
+    def Preorder(self, tree):
         # 前序遍历：根节点、左子树、右子树
         if tree:
             print(tree.RootVal)
-            self.preorder(tree.GetLeftChild())
-            self.preorder(tree.GetRightChild())
+            self.Preorder(tree.GetLeftChild())
+            self.Preorder(tree.GetRightChild())
 
-    def midorder(self, tree):
+    def Midorder(self, tree):
         # 前序遍历：根节点、左子树、右子树
         if tree:
-            self.preorder(tree.GetLeftChild())
+            self.Midorder(tree.GetLeftChild())
             print(tree.RootVal)
-            self.preorder(tree.GetRightChild())
+            self.Midorder(tree.GetRightChild())
 
-    def postorder(self, tree):
+    def Postorder(self, tree):
         # 前序遍历：根节点、左子树、右子树
         if tree:
-            self.preorder(tree.GetLeftChild())
-            self.preorder(tree.GetRightChild())
+            self.Postorder(tree.GetLeftChild())
+            self.Postorder(tree.GetRightChild())
             print(tree.RootVal)
 
 
 class BinaryHeap():
+    """
+    按序排列的完全二叉树，包括最大堆、最小堆，此处以最小堆实例化
+    """
     def __init__(self):
         self.heap_lst = [0]
         self.size = 0
 
-    def insert(self, val):
+    def Insert(self, val):
         self.size += 1
         i = self.size
         self.heap_lst.append(val)
@@ -78,8 +83,98 @@ class BinaryHeap():
             if val < self.heap_lst[i//2]:
                 self.heap_lst[i] = self.heap_lst[i // 2]
                 self.heap_lst[i // 2] = val
+            else:
+                break
             i = i // 2
         return None
+
+    def FindMin(self):
+        if self.size > 0:
+            return self.heap_lst[1]
+        return None
+
+    def FindMax(self):
+        if self.size > 0:
+            return max(self.heap_lst)
+        return None
+
+    def DelMin(self):
+        """
+        删除堆顶的最小节点，并将堆底节点填充到堆顶，然后重新调整堆顺序
+        :return:
+        """
+        # 空树无最小
+        if len(self.heap_lst) < 2:
+            print('Empty heap has no minimize node')
+            return
+
+        self.heap_lst[1] = self.heap_lst[-1]
+        self.heap_lst.pop()
+        self.size -= 1
+
+        i = 1
+        while i * 2 < self.size:
+            if i*2+1 > self.size:
+                tmp = self.heap_lst[i*2]
+                self.heap_lst[i*2] = self.heap_lst[i]
+                self.heap_lst[i] = tmp
+            elif self.heap_lst[i*2] > self.heap_lst[i*2+1]:
+                tmp = self.heap_lst[i*2+1]
+                self.heap_lst[i*2+1] = self.heap_lst[i]
+                self.heap_lst[i] = tmp
+            else:
+                tmp = self.heap_lst[i*2]
+                self.heap_lst[i*2] = self.heap_lst[i]
+                self.heap_lst[i] = tmp
+            i = i * 2
+        return
+
+    def IsEmpty(self):
+        return True if len(self.heap_lst) > 1 else False
+
+    def Size(self):
+        return len(self.heap_lst) - 1
+
+    def BuildHeap(self, lst):
+        self.size = len(lst)
+        self.heap_lst = [0] + lst
+        # j = self.size
+        # while j > 0:
+        for j in range(self.size//2, 0, -1):
+            i = j
+            while i * 2 < self.size+1:
+                if i * 2 + 1 > self.size:
+                    tmp = self.heap_lst[i*2]
+                    self.heap_lst[i*2] = self.heap_lst[i]
+                    self.heap_lst[i] = tmp
+                elif self.heap_lst[i*2] < self.heap_lst[i*2+1]:
+                    tmp = self.heap_lst[i*2]
+                    self.heap_lst[i*2] = self.heap_lst[i]
+                    self.heap_lst[i] = tmp
+                elif self.heap_lst[i*2] > self.heap_lst[i*2+1]:
+                    tmp = self.heap_lst[i*2+1]
+                    self.heap_lst[i*2+1] = self.heap_lst[i]
+                    self.heap_lst[i] = tmp
+                else:
+                    break
+                if j == 1:          # 需要再i=2重新赋值给j=1之前返回，负责进入死循环
+                    return self.heap_lst
+                i = i * 2
+
+
+class TreeNode():
+    def __init__(self, key, val, right, left, parent):
+        self.key = key
+        self.val = val
+        self.right = right
+        self.left = left
+        self.parent = parent
+
+
+class BinarySearchTree():
+    def __init__(self):
+
+        pass
 
 
 if __name__ == '__main__':
@@ -87,9 +182,35 @@ if __name__ == '__main__':
     # # print(bt.GetLeftChild().GetRootVal())
     # print(bt.preorder(bt))
 
+    """
+             0.5
+           /     \
+         1         3
+       /  \      /   \
+      2    5    6     7
+     / \ 
+    4  4.5
+    """
     bh = BinaryHeap()
-    [bh.insert(i) for i in range(1, 8)]
-    bh.insert(0.5)
-    bh.insert(4.5)
+    bh.DelMin()
+    [bh.Insert(i) for i in range(1, 8)]
+    bh.Insert(0.5)
+    bh.Insert(4.5)
 
+    """
+             1
+          /     \
+         2       3
+       /  \     /  \
+      4.5  5   6    7
+     / 
+    4  
+    """
+    bh.DelMin()
 
+    bh2 = BinaryHeap()
+    lst = [3, 7, 9, 5, 0.8, 4]
+    for x in lst:
+        bh2.Insert(x)
+    bh3 = BinaryHeap()
+    print(bh3.BuildHeap(lst))
